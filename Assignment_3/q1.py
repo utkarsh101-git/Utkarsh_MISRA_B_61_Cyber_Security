e1a866d27bb6172ad95767e1de09b67df8038cc0
Utkarsh Misra <70424785+utkarsh101-git@users.noreply.github.com>
2020-09-05 17:02:52 +0530

    Update q1.py

--- Assignment_3/q1.py
+++ Assignment_3/q1.py
@@ -1,1 +1,13 @@
+str=input("ENTER TEH STRING TO CHECK ITS PROPERTIES ")
+l=[]
+al=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
+for i in range(0,26):
+    l.append(0) 
+for i in range(0,26):
+    if al[i] in str.lower():
+        l[i]=1
+if 0 in l:
+    print("NOT A PANAGRAM")
+else:
+    print("PANAGRAM")
