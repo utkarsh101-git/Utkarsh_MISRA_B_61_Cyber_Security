b8c6af01e92ea9ab9cd03d365a5b5add55cd48cc
Utkarsh Misra <70424785+utkarsh101-git@users.noreply.github.com>
2020-11-09 12:21:14 +0530

    Add files via upload

---
+++ Assignment-4/3.js
@@ -0,0 +1,10 @@
+let GCD = (n1, n2) => {
+    while (n1 != n2) {
+        if (n1 > n2) {
+            n1 = n1 - n2;
+        } else {
+            n2 = n2 - n1;
+        }
+    }
+    return n2;
+}
---
+++ Assignment-4/4.js
@@ -0,0 +1,3 @@
+var n1 = Number(prompt("Enter first number\n"));
+var n2 = Number(prompt("Enter second number\n"));
+alert(n1 + n2);
